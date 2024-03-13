bca5be3d93618edfb0044c2a00c56a8aa0ac328e
Jantonio <ja.alcalde.2021@alumnos.urjc.es>
2024-03-13 13:32:59 +0100

    crear todas las clases

---
+++ clases/Main.java
@@ -0,0 +1,23 @@
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * 
+ */
+public class Main {
+
+    /**
+     * Default constructor
+     */
+    public Main() {
+    }
+
+    /**
+     * @param args
+     */
+    public void main(string args) {
+        // TODO implement here
+    }
+
+}
